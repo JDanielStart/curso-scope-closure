24aaca47a36dd7c2f062aeeff66ed15c13136eca
JDaniel <daniel@JDStart.onmicrosoft.com>
2023-10-31 19:06:23 +0000

    Usando el modo estricto

--- src/strict.js
+++ src/strict.js
@@ -1,10 +1,14 @@
+// En el modo estricto no se permite el hoisting
+
 "use strict";
-pi = 3.1416;
-console.log(pi);
+/* pi = 3.1416; no se podría
+console.log(pi); */
 
 function myFunction() {
     "use strict";
-    return pi = 3.1416;
+    /* return pi = 3.1416; no se podría */
+    const pi = 3.1416;
+    return pi;
 }
 
-console.log(myFunction());
+/* console.log(myFunction()); No se podría*/
